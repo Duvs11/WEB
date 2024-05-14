7ca72a059d45ff3c34419ea538feebe28c339735
Duvan Ramirez <solduvan11@gmail.com>
2024-05-14 15:30:02 -0500

    todo

---
+++ programa.js
@@ -0,0 +1,14 @@
+/*for(i=0 ; i<3 ; i++)
+{
+    alert("Hola!!")
+}*/
+
+function escribir(){
+
+    document.write(
+        for(i=1;i<=10;i++)
+            {
+                
+            }
+    )
+}
